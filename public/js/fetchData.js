ffc642126247f3836d36417b45c389c5d6095db9
Lidobix <pipoflutepouet@gmail.com>
2022-01-29 11:29:00 +0100

    Linter le code

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -3,146 +3,142 @@
  * @author VirtuoWorks
  */
 
-
-window.addEventListener('DOMContentLoaded',()=>{
+window.addEventListener('DOMContentLoaded', () => {
   /**
    * balise <p> qui contiendra le texte de réponse sur la page HTML
    * @type {HTMLElement} */
-  const pokeP=document.getElementById('pokeInfo')
-    /**
+  const pokeP = document.getElementById('pokeInfo');
+  /**
    * Div à laquelle sera attachée PokeP
    * @type {HTMLElement} */
-  const pokeDiv=document.getElementById('pokemon-info')
-    /**
+  const pokeDiv = document.getElementById('pokemon-info');
+  /**
    * Bouton d"attribution d"une compétence de combat du Pokémon
    * @type {HTMLElement} */
-  const pokeAbilityBtn=document.getElementById('ability')
-    /**
-   * Cette fonction envoie une reqûete à l"API pokeapi.co pour obtenir les infos d"un Pokemon choisi par son ID et renvoie un élément HTML (texte) contenant le nom du Pokémon.
+  const pokeAbilityBtn = document.getElementById('ability');
+  /**
+   * Cette fonction envoie une reqûete à l"API pokeapi.co pour obtenir
+   *  les infos d"un Pokemon choisi par son ID et renvoie un élément
+   * HTML (texte) contenant le nom du Pokémon.
    * L"ID est construit à partir d"un nombre aléatoire.
    *
    * @function fetchPokemon
    * @returns {HTMLElement} Texte contenant le nom du Pokemon
    */
-  const fetchPokemon=async ()=>{
-/**
+  const fetchPokemon = async () => {
+  /**
      * ID du Pokemon
      * @type {number} */
-    const pokedexNum=Math.floor(Math.random() * 897)
-   /**
-     * Réponse de l"API
+    const pokedexNum = Math.floor(Math.random() * 897);
+    /**
+    * Réponse de l"API
      * @type {string} */
-    let foundPokemon=''
-   /**
+    let foundPokemon = '';
+    /**
      * Réponse de l"API convertie en JSON
      *  @type {string} */
-    let jsonPokemon=''
+    let jsonPokemon = '';
     /**
      * Objet contenant le nom du Pokemon
      * @type {object}
      * @property {string} pokeInfo.name */
-    const pokeInfo={}
+    const pokeInfo = {};
 
-    try{
-      foundPokemon=await fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}`, {method: 'GET', headers: {'Content-Type': 'application/json'}})
-    }
-    catch(error){console.error(error.message)}
+    try {
+      foundPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokedexNum}`, { method: 'GET', headers: { 'Content-Type': 'application/json' } });
+    } catch (error) { console.error(error.message); }
 
-    if(foundPokemon){
-      try{
-        jsonPokemon=await foundPokemon.json()
-        pokeInfo.name=`${String(jsonPokemon.species.name).slice(0, 1).toUpperCase()}${String(jsonPokemon.species.name).slice(1, jsonPokemon.species.name.length).toLowerCase()}`
-      }
-      catch(error){console.error(error.message)}
+    if (foundPokemon) {
+      try {
+        jsonPokemon = await foundPokemon.json();
+        pokeInfo.name = `${String(jsonPokemon.species.name).slice(0, 1).toUpperCase()}${String(jsonPokemon.species.name).slice(1, jsonPokemon.species.name.length).toLowerCase()}`;
+      } catch (error) { console.error(error.message); }
+    } else { jsonPokemon = 'No Pokémon found...'; }
 
-    }
-    else{jsonPokemon='No Pokémon found...'}
-
-    if(pokeP.innerText !== ''){pokeP.innerText=''}
-    pokeP.innerText=`Your Pokémon is ${pokeInfo.name}.`
-    pokeAbilityBtn.removeAttribute('disabled')
-  }
+    if (pokeP.innerText !== '') { pokeP.innerText = ''; }
+    pokeP.innerText = `Your Pokémon is ${pokeInfo.name}.`;
+    pokeAbilityBtn.removeAttribute('disabled');
+  };
   /**
-   * Cette fonction envoie une reqûete à l"API pokeapi.co pour obtenir une compétence de combat, choisie par son ID et renvoie un élément HTML (texte) affichant la réponse sur la page.
+   * Cette fonction envoie une reqûete à l"API pokeapi.co pour obtenir une compétence de combat,
+   * choisie par son ID et renvoie un élément HTML (texte) affichant la réponse sur la page.
    * L"ID est construit à partir d"un nombre aléatoire.
    *
    * @function fetchPokemonAbilities
    * @returns {HTMLElement} Texte contenant la compétence de combat.
    *
    */
-  const fetchPokemonAbilities=async()=>{
-   /**
+  const fetchPokemonAbilities = async () => {
+    /**
      * ID de la compétence, construit à partir d"un nomnre aléatoire.
      * @type {number} */
-    const pokedexNum=Math.floor(Math.random() * 266)
-        /**
+    const pokedexNum = Math.floor(Math.random() * 266);
+    /**
      * Réponse de l"API.
      * @type {string} */
-    let foundAbilities=''
-       /**
+    let foundAbilities = '';
+    /**
      * balise <p> où sera affichée la compténce.
      * @type {HTMLElement} */
-    const pokeAbility=document.getElementById('pokeAbility')
-        /**
+    const pokeAbility = document.getElementById('pokeAbility');
+    /**
      * Réponse de l"API convertie en JSON, contenant la compétence.
      * @type {object} */
-    let jsonAbilities={}
-        /**
+    let jsonAbilities = {};
+    /**
      * Compétence extraite de l"objet.
      * @type {string} */
-    let abilityToDisplay=''
-  
+    let abilityToDisplay = '';
+
     try {
-      foundAbilities=await fetch(`https://pokeapi.co/api/v2/ability/${pokedexNum}`, {method: 'GET', headers: {'Content-Type': 'application/json'}})
-    }
-    catch(error){console.error(error.message)}
+      foundAbilities = await fetch(`https://pokeapi.co/api/v2/ability/${pokedexNum}`, { method: 'GET', headers: { 'Content-Type': 'application/json' } });
+    } catch (error) { console.error(error.message); }
 
-    if(foundAbilities){
-      try{
-        jsonAbilities=await foundAbilities.json();
-        if('' !== jsonAbilities.name && undefined !== jsonAbilities.name){
-          abilityToDisplay=`${String(jsonAbilities.name).slice(0, 1).toUpperCase()}${String(jsonAbilities.name).slice(1, jsonAbilities.name.length).toLowerCase()}`
-        }
-        else{abilityToDisplay='Tackle'}
-      }
-      catch(error){console.error(error.message)}
+    if (foundAbilities) {
+      try {
+        jsonAbilities = await foundAbilities.json();
+        if (jsonAbilities.name !== '' && undefined !== jsonAbilities.name) {
+          abilityToDisplay = `${String(jsonAbilities.name).slice(0, 1).toUpperCase()}${String(jsonAbilities.name).slice(1, jsonAbilities.name.length).toLowerCase()}`;
+        } else { abilityToDisplay = 'Tackle'; }
+      } catch (error) { console.error(error.message); }
+    } else { jsonAbilities = 'No ability found...'; }
 
-    }
-    else{jsonAbilities='No ability found...'}
+    if (pokeAbility.innerText !== '') { pokeAbility.innerText = ''; }
 
-    if(pokeAbility.innerText !== ''){pokeAbility.innerText=''}
-
-    pokeAbility.innerText=`It now knows the move ${abilityToDisplay}!`
-  }
-   /**
-   * Cette fonction appelle la fonction de requête de Pokemon, et affiche le texte reçu par cette dernière
+    pokeAbility.innerText = `It now knows the move ${abilityToDisplay}!`;
+  };
+  /**
+   * Cette fonction appelle la fonction de requête de Pokemon,
+   * et affiche le texte reçu par cette dernière
    *
    * @function invoquePokemon
    */
-  const invoquePokemon=()=>{
-      /**
+  const invoquePokemon = () => {
+    /**
      * Bouton "chox de Pokemon" de la page HTML.
      * @type {HTMLElement} */
-    const pokeBtn=document.getElementById('pokemon')
-    pokeBtn.addEventListener('click', fetchPokemon)
-    pokeDiv.appendChild(pokeP)
-  }
+    const pokeBtn = document.getElementById('pokemon');
+    pokeBtn.addEventListener('click', fetchPokemon);
+    pokeDiv.appendChild(pokeP);
+  };
   /**
-   * Cette fonction appelle la fonction de requête de compétence, et affiche le texte reçu par cette dernière
+   * Cette fonction appelle la fonction de requête de compétence,
+   *  et affiche le texte reçu par cette dernière
    *
    * @function pokemonAbility
    */
-  const pokemonAbility=()=>{
-    pokeAbilityBtn.addEventListener('click', fetchPokemonAbilities)
-    pokeDiv.appendChild(pokeAbility)
-  }
-    /**
+  const pokemonAbility = () => {
+    pokeAbilityBtn.addEventListener('click', fetchPokemonAbilities);
+    // eslint-disable-next-line no-undef
+    pokeDiv.appendChild(pokeAbility);
+  };
+  /**
    * IIFE qui lance les fonctions de demande de Pokemon et de compétence quand le DOM est chargé.
    *
    * @function startAll
    */
-  (function startAll(){
-    invoquePokemon()
-    pokemonAbility()
-  })()
-})
+  (function startAll() {
+    invoquePokemon();
+    pokemonAbility();
+  }());
+});
--- public/js/functionsToTest.js
+++ public/js/functionsToTest.js
@@ -1,24 +1,28 @@
+// eslint-disable-next-line no-unused-vars
 const returnAnObject = (...args) => {
   let response = {};
   if (args.length) {
     let index = 0;
-    args.forEach(arg => {
+    args.forEach((arg) => {
       response[index] = arg;
-      index++;
-    })
+      index += 1;
+    });
   } else {
     response = 'No argument was given to the function.';
-  };
+  }
   return response;
 };
 
+// eslint-disable-next-line no-unused-vars
 const multiplyAllByTwo = (arrayOfNumbers) => {
   let response;
+  // eslint-disable-next-line no-array-constructor
   if (arrayOfNumbers.constructor.prototype === new Array().constructor.prototype) {
-    response = arrayOfNumbers.map((val => val * 2));
+    response = arrayOfNumbers.map(((val) => val * 2));
+    // eslint-disable-next-line no-undef
     console.log('arrayTimesTwo: ', arrayTimesTwo);
   } else {
     response = 'The argument is not an Array of numbers';
-  };
+  }
   return response;
 };
